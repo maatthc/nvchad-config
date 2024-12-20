95d0f56a3c23658965a85d3efd557032b32a0a85
Alexandre Andrade <aleandrade@gmail.com>
2024-12-20 19:01:21 +1100

    refactor: 💡 Change terminal size

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,6 +1,7 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
+-- Or use help :h nvui
 
 ---@type ChadrcConfig
 local M = {}
@@ -12,6 +13,11 @@ M.base46 = {
 		Comment = { italic = true },
 		["@comment"] = { italic = true },
 	},
+}
+
+M.term = {
+	sizes = { sp = 0.5, vsp = 0.5, ["bo sp"] = 0.5, ["bo vsp"] = 0.5 },
+	-- sp and vsp are horizontal/vertical splits
 }
 
 return M
