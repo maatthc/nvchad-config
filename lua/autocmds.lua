e07d79cdf578c3eff5fe4398d54ee619c89e016e
Alexandre Andrade <aleandrade@gmail.com>
2025-01-10 10:38:06 +1100

    feat: 🎸 Show Nvdash when all buffers are closed

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -14,3 +14,13 @@ vim.api.nvim_create_autocmd("VimEnter", {
 	callback = open_nvim_tree,
 	group = group,
 })
+
+-- Show Nvdash when all buffers are closed 
+vim.api.nvim_create_autocmd("BufDelete", {
+  callback = function()
+    local bufs = vim.t.bufs
+    if #bufs == 1 and vim.api.nvim_buf_get_name(bufs[1]) == "" then
+      vim.cmd "Nvdash"
+    end
+  end,
+})
