37161e365137c8a69923a0540d2bd0a6da72b50d
Alexandre Andrade <aleandrade@gmail.com>
2025-02-09 18:03:52 +1100

    feat: 🎸 Ignore function keys

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,14 +1,15 @@
 require("nvchad.mappings")
 
--- add yours here
-
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>", { desc = "In Insert mode, just enter 'jk' to simulate ESC"})
+map("i", "jk", "<ESC>", { desc = "In Insert mode, just enter 'jk' to simulate ESC" })
 
 map("i", "<S-SPACE>", function()
 	vim.fn.feedkeys(vim.fn["copilot#Accept"](), "n") -- good for Ghostty terminal only
 end, { desc = "Copilot accept" })
 
+for i = 13, 30 do
+	map({ "n", "v", "i", "c" }, "<F" .. i .. ">", "<nop>", { desc = "Ignore function keys" })
+end
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
