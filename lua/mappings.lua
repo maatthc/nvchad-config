4319db44ada12395c9b9bf8e6ee41228b1f17aa7
Alexandre Andrade <aleandrade@gmail.com>
2025-02-25 16:30:11 +1100

    fix: 🐛 Kitty terminal

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,8 +6,10 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>", { desc = "In Insert mode, just enter 'jk' to simulate ESC" })
 
 map("i", "<S-SPACE>", function()
-	vim.fn.feedkeys(vim.fn["copilot#Accept"](), "n") -- good for Ghostty terminal only
+	vim.fn.feedkeys(vim.fn["copilot#Accept"](), "n") -- good for Ghostty terminal
 end, { desc = "Copilot accept" })
+--- To use with Kitty terminal, add this to your kitty.conf:
+--- map shift+enter send_text all \x1b[13;2u
 
 for i = 13, 30 do
 	map({ "n", "v", "i", "c" }, "<F" .. i .. ">", "<nop>", { desc = "Ignore function keys" })
