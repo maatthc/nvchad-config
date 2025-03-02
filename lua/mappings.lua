a37c447330b3a9e34cd8714c7d5aad35e2778ece
Alexandre Andrade <aleandrade@gmail.com>
2025-03-02 11:06:25 +1100

    fix: 🐛 Mapping for Kitty

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -9,7 +9,7 @@ map("i", "<S-SPACE>", function()
 	vim.fn.feedkeys(vim.fn["copilot#Accept"](), "n") -- good for Ghostty terminal
 end, { desc = "Copilot accept" })
 --- To use with Kitty terminal, add this to your kitty.conf:
---- map shift+enter send_text all \x1b[13;2u
+--- map shift+space send_text all \x1b[32;2u
 
 for i = 13, 30 do
 	map({ "n", "v", "i", "c" }, "<F" .. i .. ">", "<nop>", { desc = "Ignore function keys" })
