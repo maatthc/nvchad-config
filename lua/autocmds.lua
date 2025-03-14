8da55c30e5ee30bef04881122a2e084e2bca5d8a
Alexandre Andrade <aleandrade@gmail.com>
2025-03-15 10:45:52 +1100

    feat: 🎸 Restore cursor position

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,4 +1,5 @@
 local group = vim.api.nvim_create_augroup("mine", { clear = true })
+local autocmd = vim.api.nvim_create_autocmd
 
 -- When opening a directory, open a new buffer and open nvim-tree
 local function open_nvim_tree(data)
@@ -6,7 +7,7 @@ local function open_nvim_tree(data)
 	if directory then
 		vim.cmd.cd(data.file)
 		vim.cmd.enew()
-	  require("nvim-tree.api").tree.open()
+		require("nvim-tree.api").tree.open()
 	end
 end
 
@@ -15,12 +16,28 @@ vim.api.nvim_create_autocmd("VimEnter", {
 	group = group,
 })
 
--- Show Nvdash when all buffers are closed 
-vim.api.nvim_create_autocmd("BufDelete", {
-  callback = function()
-    local bufs = vim.t.bufs
-    if #bufs == 1 and vim.api.nvim_buf_get_name(bufs[1]) == "" then
-      vim.cmd "Nvdash"
-    end
-  end,
+-- Show Nvdash when all buffers are closed
+autocmd("BufDelete", {
+	callback = function()
+		local bufs = vim.t.bufs
+		if #bufs == 1 and vim.api.nvim_buf_get_name(bufs[1]) == "" then
+			vim.cmd("Nvdash")
+		end
+	end,
+})
+
+-- Restore cursor position
+autocmd("BufReadPost", {
+	pattern = "*",
+	callback = function()
+		local line = vim.fn.line("'\"")
+		if
+			line > 1
+			and line <= vim.fn.line("$")
+			and vim.bo.filetype ~= "commit"
+			and vim.fn.index({ "xxd", "gitrebase" }, vim.bo.filetype) == -1
+		then
+			vim.cmd('normal! g`"')
+		end
+	end,
 })
