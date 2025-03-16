9d39a3d5b5ac319ffb1c07a77526abf926ba0320
Alexandre Andrade <aleandrade@gmail.com>
2025-03-16 13:21:43 +1100

    fix: 🐛 Classes dont work

--- lua/configs/treesitter-textobjects.lua
+++ lua/configs/treesitter-textobjects.lua
@@ -8,10 +8,8 @@ local textobjects = {
 			-- You can use the capture groups defined in textobjects.scm
 			["af"] = { query = "@function.outer", desc = "Select outer part of a class region" },
 			["if"] = { query = "@function.inner", desc = "Select inner part of a class region" },
-			["ac"] = { query = "@class.outer", desc = "Select outer part of a class region" },
 			-- You can optionally set descriptions to the mappings (used in the desc parameter of
 			-- nvim_buf_set_keymap) which plugins like which-key display
-			["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
 			-- You can also use captures from other query groups like `locals.scm`
 			["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
 		},
@@ -31,10 +29,6 @@ local textobjects = {
 		-- extended to include preceding or succeeding whitespace. Succeeding
 		-- whitespace has priority in order to act similarly to eg the built-in
 		-- `ap`.
-		--
-		-- Can also be a function which gets passed a table with the keys
-		-- * query_string: eg '@function.inner'
-		-- * selection_mode: eg 'v'
 		-- and should return true or false
 		include_surrounding_whitespace = false,
 	},
