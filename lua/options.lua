c8f2e69a03e515b64ebffb21013dc150fc0fadca
Alexandre Andrade <aleandrade@gmail.com>
2025-03-19 11:20:59 +1100

    feat: 🎸 Clipboard for macos via ssh

--- lua/options.lua
+++ lua/options.lua
@@ -9,3 +9,16 @@ o.foldcolumn = "1" -- '0' is not bad
 o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 o.foldlevelstart = 99
 o.foldenable = true
+
+-- To force Nvim to use the OSC 52 provider for clipboard
+vim.g.clipboard = {
+	name = "OSC 52",
+	copy = {
+		["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+		["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+	},
+	paste = {
+		["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+		["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+	},
+}
