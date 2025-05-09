a5121fa742a8a06102c6f76a85d36aba8673c525
Alexandre Andrade <aleandrade@gmail.com>
2025-05-09 20:20:51 +1000

    feat: 🎸 Animated fire cursor

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -181,4 +181,18 @@ return {
 			require("nvim-surround").setup({})
 		end,
 	},
+
+	{
+		"sphamba/smear-cursor.nvim",
+		event = "VeryLazy",
+		opts = {
+			cursor_color = "#ff8800",
+			stiffness = 0.3,
+			trailing_stiffness = 0.1,
+			trailing_exponent = 5,
+			never_draw_over_target = true,
+			hide_target_hack = true,
+			gamma = 1,
+		},
+	},
 }
