4a0c3704b1a3e96d77b14e7662cfbd5884a5920d
Alexandre Andrade <aleandrade@gmail.com>
2025-05-09 20:33:24 +1000

    feat: 🎸 Better animation

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -187,8 +187,8 @@ return {
 		event = "VeryLazy",
 		opts = {
 			cursor_color = "#ff8800",
-			stiffness = 0.3,
-			trailing_stiffness = 0.1,
+			stiffness = 0.8,
+			trailing_stiffness = 0.3,
 			trailing_exponent = 5,
 			never_draw_over_target = true,
 			hide_target_hack = true,
