307b66cf47e92e59bc775a324a8977daf1f83fed
Alexandre Andrade <aleandrade@gmail.com>
2025-06-04 08:10:23 +1000

    refactor: 💡 Use web icons

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -208,7 +208,7 @@ return {
 	{
 		"MeanderingProgrammer/render-markdown.nvim",
 		event = "VeryLazy",
-		dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" }, -- if you use standalone mini plugins
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
 		---@module 'render-markdown'
 		opts = require("configs.render-markdown"),
 	},
