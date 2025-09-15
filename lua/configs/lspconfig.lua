79a01d93e356d6f782ac767be704c20c57923a05
Alexandre Andrade <aleandrade@gmail.com>
2025-09-14 22:37:10 -0300

    feat: 🎸 Use PyLsp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -3,7 +3,7 @@ require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require("lspconfig")
 
-local servers = { "html", "cssls", "zls", "eslint", "ts_ls", "jedi_language_server", "gopls" }
+local servers = { "html", "cssls", "zls", "eslint", "ts_ls", "pylsp", "gopls" }
 local nvlsp = require("nvchad.configs.lspconfig")
 
 -- lsps with default config
@@ -14,3 +14,18 @@ for _, lsp in ipairs(servers) do
 		capabilities = nvlsp.capabilities,
 	})
 end
+-- custom lsp configs
+lspconfig.pylsp.setup({
+	on_attach = nvlsp.on_attach,
+	on_init = nvlsp.on_init,
+	capabilities = nvlsp.capabilities,
+	settings = {
+		pylsp = {
+			plugins = {
+				pycodestyle = {
+					ignore = { "E501", "W503" },
+				},
+			},
+		},
+	},
+})
