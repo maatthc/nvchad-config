8dcfa246eb1693f51836283b3ebb763e40af85ac
Alexandre Andrade <aleandrade@gmail.com>
2025-10-09 17:47:09 +1100

    refactor: 💡 Use vim.lsp.config

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,21 +1,11 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require("lspconfig")
-
 local servers = { "html", "cssls", "zls", "eslint", "ts_ls", "pylsp", "gopls" }
 local nvlsp = require("nvchad.configs.lspconfig")
 
--- lsps with default config
-for _, lsp in ipairs(servers) do
-	lspconfig[lsp].setup({
-		on_attach = nvlsp.on_attach,
-		on_init = nvlsp.on_init,
-		capabilities = nvlsp.capabilities,
-	})
-end
 -- custom lsp configs
-lspconfig.pylsp.setup({
+vim.lsp.config("pylsp", {
 	on_attach = nvlsp.on_attach,
 	on_init = nvlsp.on_init,
 	capabilities = nvlsp.capabilities,
@@ -29,3 +19,5 @@ lspconfig.pylsp.setup({
 		},
 	},
 })
+
+vim.lsp.enable(servers)
