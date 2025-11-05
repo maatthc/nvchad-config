f3594a1330f7689370204e40538554949fe37b2a
Alexandre Andrade <aleandrade@gmail.com>
2025-11-05 22:26:02 +1100

    refactor: 💡 Remove cursor - using Kitty now

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -202,20 +202,6 @@ return {
 	},
 
 	{
-		"sphamba/smear-cursor.nvim",
-		event = "VeryLazy",
-		opts = {
-			cursor_color = "#ff8800",
-			stiffness = 0.8,
-			trailing_stiffness = 0.3,
-			trailing_exponent = 5,
-			never_draw_over_target = true,
-			hide_target_hack = true,
-			gamma = 1,
-		},
-	},
-
-	{
 		"MeanderingProgrammer/render-markdown.nvim",
 		event = "VeryLazy",
 		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
