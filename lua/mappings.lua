bfc6983f97b0bd75ae1a816c6196697ebd2070ce
Alexandre Andrade <aleandrade@gmail.com>
2025-12-31 16:13:31 +1100

    feat: 🎸 Add Terminal nav

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -56,10 +56,10 @@ map("n", "<A-j>", require("smart-splits").resize_down)
 map("n", "<A-k>", require("smart-splits").resize_up)
 map("n", "<A-l>", require("smart-splits").resize_right)
 
-map("n", "<C-h>", require("smart-splits").move_cursor_left)
-map("n", "<C-j>", require("smart-splits").move_cursor_down)
-map("n", "<C-k>", require("smart-splits").move_cursor_up)
-map("n", "<C-l>", require("smart-splits").move_cursor_right)
+map({ "n", "t" }, "<C-h>", require("smart-splits").move_cursor_left)
+map({ "n", "t" }, "<C-j>", require("smart-splits").move_cursor_down)
+map({ "n", "t" }, "<C-k>", require("smart-splits").move_cursor_up)
+map({ "n", "t" }, "<C-l>", require("smart-splits").move_cursor_right)
 
 -- Terminal splitting and resizing END
 
