60a8037adc608e87b8fc0047290db08779fb629e
Alexandre Andrade <aleandrade@gmail.com>
2026-01-12 06:14:04 +1100

    feat: 🎸 Add vscode-diff

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -290,4 +290,13 @@ return {
 			},
 		},
 	},
+
+	{
+		"esmuellert/vscode-diff.nvim",
+		dependencies = { "MunifTanjim/nui.nvim" },
+		cmd = "CodeDiff",
+		config = function()
+			require("configs.git-diff")
+		end,
+	},
 }
