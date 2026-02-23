f86d72cce39bddb9aa77f95948bb1ec3b538c4ad
Alexandre Andrade <aleandrade@gmail.com>
2026-02-24 07:08:58 +1100

    feat: 🎸 Add mini.move

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -299,4 +299,27 @@ return {
 			require("configs.git-diff")
 		end,
 	},
+
+	-- Move code around
+	{
+		"nvim-mini/mini.move",
+		lazy = false,
+		version = "*",
+		config = function()
+			require("mini.move").setup({
+				mappings = {
+					left = "<A-h>",
+					right = "<A-l>",
+					down = "<A-j>",
+					up = "<A-k>",
+
+					-- Move current line in Normal mode
+					line_left = "<A-h>",
+					line_right = "<A-l>",
+					line_down = "<A-j>",
+					line_up = "<A-k>",
+				},
+			})
+		end,
+	},
 }
